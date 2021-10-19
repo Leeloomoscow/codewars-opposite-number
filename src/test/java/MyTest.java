3424154578546828a5796fec27754b54c6e53cad
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-10-19 19:42:43 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,17 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void tests() {
+        assertEquals(-1, Kata.opposite(1));
+    }
 
+    private static class Kata {
+        public static int opposite(int number) {
+            int result = -1 * (number);
+            return result;
+        }
     }
 }
